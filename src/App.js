99871923dbfb720217ccaf0823bb478786dec5ec
Victor <94798862+victorhe33@users.noreply.github.com>
2022-06-18 01:34:28 -0400

    Init Hello world via functional components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
+import Header from './components/Header'
+import style from './app.css'
+//for class based
+// import React from 'react' 
+
+//FUNCTION BASED COMPONENT
 function App() {
+  const name = 'Victor';
+  const x = false;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello From React</h1>
+      <h2>Hello {x ? 'Yes' : 'No'} {name}</h2>
+      <Header />
     </div>
   );
 }
+
+//CLASS BASED COMPONENT
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello from a classbased world</h1>
+//   }
+// }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>HELLO GIO!</h1>
+    </header>
+  )
+}
+
+export default Header
